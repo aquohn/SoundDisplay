d5125b24194972ea1fd0e20500fe234a4870ec75
John Khoo <john_khoo@u.nus.edu>
2019-10-07 16:13:40 +0800

    Complete OLED connections

--- SoundDisplay.srcs/sources_1/new/Top_Student.v
+++ SoundDisplay.srcs/sources_1/new/Top_Student.v
@@ -22,18 +22,28 @@ module Top_Student (
     output J_MIC3_Pin1,   // Connect to this signal from Audio_Capture.v
     output J_MIC3_Pin4,    // Connect to this signal from Audio_Capture.v
     
-    input mic_sw           // Switch to use microphone (1) or to use zero signal (0)
+    input audio_sw,           // Switch to use microphone (1) or to use zero signal (0)
+    
+    input oled_reset        // Reset signal for OLED
     );
 
     reg [11:0] mic_in;
+    reg [15:0] oled_data;
+    reg oled_reset_pipe;
     wire clk20k, clk6p25m;
     
+    initial begin
+        oled_data = 16'h07E0;
+    end
+    
     clk_voice clk_voice_mod (.clk_in(clk_in), .clk_out(clk6p25m));
-    Audio_Capture mic_capture (.CLK(clk_in), .cs(clk6p25m), .MISO(J_MIC3_Pin3),
+    Audio_Capture audio_capture (.CLK(clk_in), .cs(clk6p25m), .MISO(J_MIC3_Pin3),
     .clk_samp(J_MIC3_Pin1), .sclk(J_MIC3_Pin4), .sample(mic_in));
     
     //TODO: Complete audio part here...
     
+    clk_oled clk_oled_mod (.clk_in(clk_in), .clk_out(clk20k));
+    Oled_Display oled_display (.clk(clk6p25m), .reset(oled_reset), .pixel_data(oled_data));
     
     
     /*
