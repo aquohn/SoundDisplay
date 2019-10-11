8b7bbb24331e37487cb45a31ec9a756310063d79
lackmoo <laujiajunandrew@yahoo.com.sg>
2019-10-12 02:36:32 +0800

    Implement Task 1A

--- SoundDisplay.srcs/sources_1/new/Top_Student.v
+++ SoundDisplay.srcs/sources_1/new/Top_Student.v
@@ -6,8 +6,8 @@
 //
 //  LAB SESSION DAY (Delete where applicable): MONDAY P.M
 //
-//  STUDENT A NAME: Andrew Lau
-//  STUDENT A MATRICULATION NUMBER: 
+//  STUDENT A NAME: Andrew Lau Jia Jun
+//  STUDENT A MATRICULATION NUMBER: A0182815B
 //
 //  STUDENT B NAME: John Khoo
 //  STUDENT B MATRICULATION NUMBER: A0190732H 
@@ -23,7 +23,8 @@ module Top_Student (
     output J_MIC3_Pin1,   // Connect to this signal from Audio_Capture.v
     output J_MIC3_Pin4,    // Connect to this signal from Audio_Capture.v
     
-    input audio_sw,           // Switch to use microphone (1) or to use zero signal (0)
+    input sw0,       // Switch to use microphone (1) or to use zero signal (0)
+    output [11:0] led,    // led to display the input value from mic_in
     
     input oled_reset,        // Reset signal for OLED
     output [7:0] JB        // Control signals to OLED
@@ -38,13 +39,16 @@ module Top_Student (
         oled_data = 16'h07E0;
     end
     
+    //Audio part
     clk_voice clk_voice_mod (.clk_in(clk_in), .clk_out(clk20k));
-    Audio_Capture audio_capture (.CLK(clk_in), .cs(clk6p25m), .MISO(J_MIC3_Pin3),
-    .clk_samp(J_MIC3_Pin1), .sclk(J_MIC3_Pin4), .sample(mic_in));
     
-    //TODO: Complete audio part here...
+    Audio_Capture audio_capture (.CLK(clk_in), .cs(clk20k), .MISO(J_MIC3_Pin3),
+        .clk_samp(J_MIC3_Pin1), .sclk(J_MIC3_Pin4), .sample(mic_in));
     
-    assign oled_reset_signal = ~oled_reset_ff & oled_reset_pipe;
+    assign led = (sw0 == 1) ? 0 : mic_in;
+    
+    //oled part
+    /*assign oled_reset_signal = ~oled_reset_ff & oled_reset_pipe;
     clk_oled clk_oled_mod (.clk_in(clk_in), .clk_out(clk6p25m));
     Oled_Display oled_display (.clk(clk6p25m), .reset(oled_reset_pipe), .pixel_data(oled_data),
     .cs(JB[0]), .sdin(JB[1]), .sclk(JB[3]), .d_cn(JB[4]), .resn(JB[5]), .vccen(JB[6]), .pmoden(JB[7]));
@@ -52,7 +56,7 @@ module Top_Student (
     always @(posedge clk6p25m) begin
         oled_reset_pipe <= oled_reset;
         oled_reset_ff <= oled_reset_pipe;
-    end
+    end*/
     
     /*
     input clk, reset;
