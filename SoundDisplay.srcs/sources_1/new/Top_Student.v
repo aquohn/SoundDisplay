ee70c5a80246a822b66a49f76b3bffb889a034ce
John Khoo <john_khoo@u.nus.edu>
2019-10-14 16:51:08 +0800

    Configure system to use all inputs and outputs

--- SoundDisplay.srcs/sources_1/new/Top_Student.v
+++ SoundDisplay.srcs/sources_1/new/Top_Student.v
@@ -23,42 +23,63 @@ module Top_Student (
     output J_MIC3_Pin1,   // Connect to this signal from Audio_Capture.v
     output J_MIC3_Pin4,    // Connect to this signal from Audio_Capture.v
     
-    input [15:0] sw,       // Switch to use microphone (1) or to use zero signal (0)
-    output [11:0] led,    // led to display the input value from mic_in
+    input [15:0] sw,
+    input btnC,
+    input btnL,
+    input btnR,
+    input btnU,
+    input btnD,
     
-    input oled_reset,        // Reset signal for OLED
+    output [15:0] led,
+    output [6:0] seg,
+    output reg [3:0] an,
+    output dp,
+    
     output [7:0] JB        // Control signals to OLED
 
     );
     
     reg [3:0] system_mode = 4'b0000;
-
+    reg btnC_pipe, btnC_ff, btnU_pipe, btnU_ff, btnR_pipe, btnR_ff, btnL_pipe, btnL_ff, btnD_pipe, btnD_ff;
+    wire btnC_signal, btnU_signal, btnR_signal, btnL_signal, btnD_signal;
     wire clk20k, clk6p25m;
+    
     wire [11:0] mic_in; //data from mic
     
     reg [15:0] oled_data;
-    reg oled_reset_pipe, oled_reset_ff;
-    wire sample_pixel, oled_reset_signal;
+    wire sample_pixel;
     wire [12:0] pixel_index;
+    
+    //Common part
+    assign btnC_signal = ~btnC_ff & btnC_pipe;
+    assign btnU_signal = ~btnU_ff & btnC_pipe;
+    assign btnR_signal = ~btnR_ff & btnC_pipe;
+    assign btnL_signal = ~btnL_ff & btnC_pipe;
+    assign btnD_signal = ~btnD_ff & btnD_pipe;
     
     //Audio part
     clk_voice clk_voice_mod (.clk_in(clk_in), .clk_out(clk20k));
-    
     Audio_Capture audio_capture (.CLK(clk_in), .cs(clk20k), .MISO(J_MIC3_Pin3),
         .clk_samp(J_MIC3_Pin1), .sclk(J_MIC3_Pin4), .sample(mic_in));
-    
     assign led = (sw[0] == 1) ? 0 : mic_in;
     
     //oled part
-    assign oled_reset_signal = ~oled_reset_ff & oled_reset_pipe;
     clk_oled clk_oled_mod (.clk_in(clk_in), .clk_out(clk6p25m));
-    Oled_Display oled_display (.clk(clk6p25m), .reset(oled_reset_signal), .pixel_data(oled_data),
+    Oled_Display oled_display (.clk(clk6p25m), .reset(btnC_signal), .pixel_data(oled_data),
     .cs(JB[0]), .sdin(JB[1]), .sclk(JB[3]), .d_cn(JB[4]), .resn(JB[5]), .vccen(JB[6]), .pmoden(JB[7]),
     .sample_pixel(sample_pixel), .pixel_index(pixel_index));
     
     always @(posedge clk6p25m) begin
-        oled_reset_pipe <= oled_reset;
-        oled_reset_ff <= oled_reset_pipe;
+        btnC_pipe <= btnC;
+        btnC_ff <= btnC_pipe;
+        btnU_pipe <= btnU;
+        btnU_ff <= btnU_pipe;
+        btnL_pipe <= btnL;
+        btnL_ff <= btnL_pipe;
+        btnL_pipe <= btnL;
+        btnL_ff <= btnL_pipe;
+        btnD_pipe <= btnD;
+        btnD_ff <= btnD_pipe;
         
         oled_data <= (pixel_index < 13'd96) ? {5'b00000, 6'b000000, 5'b11111} : 13'b0;
     end
