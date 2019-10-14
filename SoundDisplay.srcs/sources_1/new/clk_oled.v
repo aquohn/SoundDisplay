cbc1a38f769c8a08d008f181356d7923ef25b950
John Khoo <john_khoo@u.nus.edu>
2019-10-14 15:40:23 +0800

    Add oled_tests.v

---
+++ SoundDisplay.srcs/sources_1/new/clk_oled.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.10.2019 14:47:23
+// Design Name: 
+// Module Name: clk_voice
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+//TODO: integrate reset?
+
+module clk_oled(
+    input clk_in,
+    output reg clk_out
+    );
+    
+    reg [11:0] cnt;
+    
+    initial begin
+        cnt = 4'd0;
+        clk_out = 1'b0; 
+    end
+    
+    parameter CNT_TOGGLE = 4'd7;
+    
+    always @(posedge clk_in) begin
+        if (cnt == CNT_TOGGLE) begin
+            cnt <= 4'd0;
+            clk_out <= ~clk_out;
+        end else begin
+            cnt <= cnt + 1;
+        end
+    end
+endmodule
